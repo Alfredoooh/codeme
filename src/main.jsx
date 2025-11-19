96b128c837aac8a3c3c0da554bd5265ff1a172fb
Alfredoooh <alfredopjonas@gmail.com>
2025-11-19 19:22:32 +0100

    Atualizar o main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx"; // <-- importante: usar .jsx e o mesmo case
+import "./index.css"; // se tiveres CSS
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+createRoot(document.getElementById("root")).render(<App />);
