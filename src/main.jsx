81c6b520db1db73f1f4ae572e81ee11d7eab79b7
Alfredoooh <alfredopjonas@gmail.com>
2026-04-15 14:59:01 +0100

    Atualizar o main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-const statusBarHeight = 44
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div style={{
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      right: 0,
-      height: statusBarHeight,
-      zIndex: 9999,
-      backgroundColor: 'transparent',
-      pointerEvents: 'none',
-    }} />
-    <div style={{ paddingTop: statusBarHeight }}>
-      <App />
-    </div>
+    <App />
   </StrictMode>
 )
