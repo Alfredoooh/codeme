b07a56eeec57d9654ca2053d4ec3b0d8218359cd
Alfredoooh <alfredopjonas@gmail.com>
2026-08-29 14:26:23 +0100

    update authscreens.dart

--- lib/authscreens.dart
+++ lib/authscreens.dart
@@ -3,6 +3,7 @@
 // ══════════════════════════════════════════════════════════════
 import 'dart:math' as math;
 import 'package:flutter/cupertino.dart';
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
