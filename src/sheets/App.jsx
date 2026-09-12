5baa896b7ca112f9fec0d062f9783a7c17fe53b4
Alfredoooh <alfredopjonas@gmail.com>
2026-09-12 20:30:15 +0100

    update

--- src/sheets/App.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import StubApp from '../shared/StubApp.jsx';
-
-export default function SheetsApp({ isDark }) {
-  return <StubApp isDark={isDark} appLabel="Folha de Cálculo" appColor="#23A63F" />;
-}
--- src/slides/App.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import StubApp from '../shared/StubApp.jsx';
-
-export default function SlidesApp({ isDark }) {
-  return <StubApp isDark={isDark} appLabel="Apresentações" appColor="#FB6704" />;
-}
